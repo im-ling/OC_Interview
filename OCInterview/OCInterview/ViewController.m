0f6d7975b8bdad0304d995401897a68757cb107b
NowOrNever <tobeabetterman_dl@163.com>
2021-07-05 05:41:30 +0800

    feat: block test

--- OCInterview/OCInterview/ViewController.m
+++ OCInterview/OCInterview/ViewController.m
@@ -16,6 +16,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self testBlock];
+}
+
+- (void)testBlock {
+    NSMutableArray *arr = [NSMutableArray arrayWithObjects:@1,@2, nil];
+//    __block NSMutableArray *arr = [NSMutableArray arrayWithObjects:@1,@2, nil];
+    void(^block)(void) = ^{
+        NSLog(@"block start");
+        for (NSObject *obj in arr) {
+            NSLog(@"%@", obj);
+        };
+    };
+    [arr addObject:@3];
+    arr = nil;
+    block();
 }
 
 
