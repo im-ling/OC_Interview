798a342466d96659ade2a62aaaecc690dbd8411c
NowOrNever <tobeabetterman_dl@163.com>
2021-07-05 01:39:07 +0800

    feat: __bridge使用

--- OCInterview/OCInterview/ViewController.m
+++ OCInterview/OCInterview/ViewController.m
@@ -16,6 +16,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    void *p = NULL;
+    {
+        NSObject *obj = [[NSObject alloc] init];
+        NSLog(@"%p", obj);
+        p = (__bridge_retained void *)obj;
+    }
+    NSLog(@"%p %@", p, p);
+//    CFRelease(p);
+    {
+        id obj = (__bridge_transfer NSObject *)p;
+    }
+//    NSLog(@"%p %@", p, p);
 }
 
 
