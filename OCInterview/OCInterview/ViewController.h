4be100a341c8d33546cbcd2d0b07bd24631d801c
NowOrNever <tobeabetterman_dl@163.com>
2021-07-05 05:35:53 +0800

    feat: 单例

--- OCInterview/OCInterview/ViewController.h
+++ OCInterview/OCInterview/ViewController.h
@@ -9,6 +9,7 @@
 
 @interface ViewController : UIViewController
 
++ (instancetype)sharedVC;
 
 @end
 
--- OCInterview/OCInterview/ViewController.m
+++ OCInterview/OCInterview/ViewController.m
@@ -13,9 +13,17 @@
 
 @implementation ViewController
 
++ (instancetype)sharedVC{
+    static ViewController* _vc;
+    static dispatch_once_t token;
+    dispatch_once(&token, ^{
+        _vc = [[ViewController alloc] init];
+    });
+    return _vc;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
 
