160f82b52d0f579ffb468e90ff3624dcf1d9b631
NowOrNever <tobeabetterman_dl@163.com>
2021-07-12 03:54:45 +0800

    feat: 响应链，事件穿透

--- OCInterview/OCInterview/ViewController.m
+++ OCInterview/OCInterview/ViewController.m
@@ -7,6 +7,16 @@
 
 #import "ViewController.h"
 
+@interface TestView: UIView
+
+@end
+@implementation TestView
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    NSLog(@"touch testview");
+}
+@end
+
+
 @interface ViewController ()
 
 @end
@@ -15,8 +25,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    TestView *view = [[TestView alloc] initWithFrame:self.view.frame];
+    view.backgroundColor = [UIColor systemBlueColor];
+    [self.view addSubview:view];
+    
+    UIView *redView = [[UIView alloc] initWithFrame:self.view.frame];
+    redView.backgroundColor = [UIColor systemRedColor];
+    redView.userInteractionEnabled = FALSE;
+    [self.view addSubview:redView];
+    
     // Do any additional setup after loading the view.
 }
+
 
 
 @end
