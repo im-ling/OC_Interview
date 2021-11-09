6b3a2326e2c92ee12c03a47423398e78b8f37210
Li Ling <liling12@staff.sina.com>
2021-11-09 14:40:50 +0800

    wip: 富文本

--- OCInterview/OCInterview/ViewController.m
+++ OCInterview/OCInterview/ViewController.m
@@ -15,7 +15,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    UILabel *label = [[UILabel alloc] init];
+//    label.numberOfLines = 2;
+//    label.attributedText = [self attStr];
+//    [label sizeToFit];
+//    [self.view addSubview:label];
+//    label.center = self.view.center;
     // Do any additional setup after loading the view.
+    
+    
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    [btn setAttributedTitle:[self attStr] forState:UIControlStateNormal];
+    btn.titleLabel.numberOfLines = 2;
+//    [btn sizeToFit];
+    btn.center = self.view.center;
+    [self.view addSubview:btn];
+    
+}
+
+
+
+
+- (NSAttributedString *)attStr{
+    NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc]init];
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:@"Title\n"];
+    [title addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"PingFangSC-Regular" size:18] range:NSMakeRange(0, title.length)];
+    [title addAttribute:NSForegroundColorAttributeName value:[UIColor blackColor] range:NSMakeRange(0, title.length)];
+    
+    NSMutableAttributedString *desc = [[NSMutableAttributedString alloc] initWithString:@"Desc"];
+    [desc addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"PingFangSC-Regular" size:12] range:NSMakeRange(0, desc.length)];
+    
+    [attStr appendAttributedString:title];
+    [attStr appendAttributedString:desc];
+    
+    //3.设置段落样式
+    NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc]init];
+    paragraph.lineSpacing = 10;  //行间距
+//    paragraph.paragraphSpacing = 20;  //段间距
+    paragraph.alignment = NSTextAlignmentCenter; //对齐方式
+//    paragraph.firstLineHeadIndent = 30;  //首行缩进
+    [attStr addAttribute:NSParagraphStyleAttributeName value:paragraph range:NSMakeRange(0, attStr.length)];
+    return attStr;
 }
 
 
