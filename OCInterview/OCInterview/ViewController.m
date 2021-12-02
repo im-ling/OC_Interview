c837d78ab49fef3990207eb7374d259629f18b5d
ling <tobeabetterman_dl@163.com>
2021-12-02 17:30:13 +0800

    feat: lottie

--- OCInterview/OCInterview/ViewController.m
+++ OCInterview/OCInterview/ViewController.m
@@ -6,16 +6,29 @@
 //
 
 #import "ViewController.h"
-
+#import <Lottie/Lottie.h>
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blackColor];
     // Do any additional setup after loading the view.
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    LOTAnimationView *lotiView = [LOTAnimationView animationNamed:@"wbl_lottie_countdown"];
+    lotiView.contentMode = UIViewContentModeScaleAspectFill;
+    lotiView.completionBlock = ^(BOOL animationFinished) {
+        NSLog(@"123");
+    };
+    lotiView.frame = self.view.frame;
+    [self.view addSubview:lotiView];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [lotiView play];
+    });
 }
 
 
