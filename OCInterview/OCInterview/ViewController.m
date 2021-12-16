0bd51b5aa37c1a3d55205030431b86dcc9ff98f5
ling <tobeabetterman_dl@163.com>
2021-12-16 15:51:33 +0800

    wip

--- OCInterview/OCInterview/ViewController.m
+++ OCInterview/OCInterview/ViewController.m
@@ -20,12 +20,18 @@
 
 - (void)viewDidAppear:(BOOL)animated{
     LOTAnimationView *lotiView = [LOTAnimationView animationNamed:@"wbl_lottie_countdown"];
-    lotiView.contentMode = UIViewContentModeScaleAspectFill;
+    lotiView.contentMode = UIViewContentModeCenter;
     lotiView.completionBlock = ^(BOOL animationFinished) {
         NSLog(@"123");
     };
-    lotiView.frame = self.view.frame;
+//    lotiView.frame = self.view.frame;
+    lotiView.frame = CGRectMake(100, 100, 85, 85);
+    lotiView.center = self.view.center;
     [self.view addSubview:lotiView];
+//    [lotiView play];
+//    [lotiView pause];
+//    [lotiView stop];
+//    [lotiView play];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [lotiView play];
     });
