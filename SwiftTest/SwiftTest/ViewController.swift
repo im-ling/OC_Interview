6faa22d19a07dcdf88ba0e6de6abd8b2ef658f30
ling <tobeabetterman_dl@163.com>
2022-11-12 00:31:45 +0800

    feat: generate QR code

--- SwiftTest/SwiftTest/ViewController.swift
+++ SwiftTest/SwiftTest/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+        if let QRFilter = CIFilter(name: "CIQRCodeGenerator") {
+            QRFilter.setValue(data, forKey: "inputMessage")
+            guard let QRImage = QRFilter.outputImage else {return nil}
+            return UIImage(ciImage: QRImage)
+        }
+        return nil
+    }
+    
+    var QRCodeImageView = UIImageView.init()
+    
+    
+    func testShowQRCode() {
+        let testUrl = "https://www.baidu.com/"
+        if let image = generateQRCode(from: testUrl) {
+            view.addSubview(QRCodeImageView)
+            let sideLength = UIScreen.main.bounds.size.width
+            QRCodeImageView.image = image
+            QRCodeImageView.bounds = CGRect(x: 0, y: 0, width: sideLength, height: sideLength)
+            QRCodeImageView.center = view.center
+            
+            // 防止放大后不清晰
+            QRCodeImageView.layer.magnificationFilter = CALayerContentsFilter.nearest
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.white
+        testShowQRCode()
     }
 
 
 }
-
