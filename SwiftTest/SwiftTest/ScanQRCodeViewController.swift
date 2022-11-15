734ce134787ebe75e51d45ec6f406b42f42aa71c
ling <tobeabetterman_dl@163.com>
2022-11-15 11:15:51 +0800

    feat: add torch

--- SwiftTest/SwiftTest/ScanQRCodeViewController.swift
+++ SwiftTest/SwiftTest/ScanQRCodeViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 import AVFoundation
 
+let iconWidth = 50.0
+
 class ScanQRCodeViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     var captureSession: AVCaptureSession!
     var previewLayer: AVCaptureVideoPreviewLayer!
@@ -19,8 +21,48 @@ class ScanQRCodeViewController: UIViewController, AVCaptureMetadataOutputObjects
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
         view.backgroundColor = UIColor.black
+        setUpQRSession()
+
+        
+        let btn = UIButton.init()
+        btn.backgroundColor = UIColor.init(white: 0.9, alpha: 0.4)
+        btn.layer.cornerRadius = 10
+        btn.layer.masksToBounds = true
+        view.addSubview(btn)
+        btn.frame = CGRect(x: 50, y: view.bounds.size.height * 0.7, width: iconWidth, height: iconWidth)
+        btn.setImage(UIImage.init(named: "flash_light"), for: UIControl.State.normal)
+        btn.addTarget(self, action: #selector(toggleFlash), for: UIControl.Event.touchUpInside)
+    }
+
+
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+    
+    // MARK: - QR SCAN
+    func setUpQRSession() {
         captureSession = AVCaptureSession()
 
         guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
@@ -58,7 +100,7 @@ class ScanQRCodeViewController: UIViewController, AVCaptureMetadataOutputObjects
 
         captureSession.startRunning()
     }
-
+    
     func failed() {
         let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
@@ -66,21 +108,6 @@ class ScanQRCodeViewController: UIViewController, AVCaptureMetadataOutputObjects
         captureSession = nil
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        if (captureSession?.isRunning == false) {
-            captureSession.startRunning()
-        }
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-
-        if (captureSession?.isRunning == true) {
-            captureSession.stopRunning()
-        }
-    }
 
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         captureSession.stopRunning()
@@ -100,11 +127,28 @@ class ScanQRCodeViewController: UIViewController, AVCaptureMetadataOutputObjects
         completeBlock(code)
     }
 
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .portrait
+    // MARK: - 手电筒
+    @objc func toggleFlash() {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { return }
+
+        do {
+            try device.lockForConfiguration()
+
+            if (device.torchMode == AVCaptureDevice.TorchMode.on) {
+                device.torchMode = AVCaptureDevice.TorchMode.off
+            } else {
+                do {
+                    try device.setTorchModeOn(level: 1.0)
+                } catch {
+                    print(error)
+                }
+            }
+
+            device.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
     }
 }
